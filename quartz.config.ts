fad500f121e46dfac434b6ba057b49d05a1db6f3
Chris M. Shinkle <cmshinkle@gmail.com>
2026-02-21 21:29:06 -0500

    Update baseUrl to chrisshinkle.com

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "cmshinkle.github.io/published-notes",
+    baseUrl: "chrisshinkle.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
